065bef48db989887604b98abf2d24df32b99c581
Alex Suraci <asuraci@pivotal.io>
2018-03-06 15:08:37 -0500

    add AuthorizedKeys

---
+++ authorized_keys.go
@@ -0,0 +1,36 @@
+package flag
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"golang.org/x/crypto/ssh"
+)
+
+type AuthorizedKeys struct {
+	Keys []ssh.PublicKey
+}
+
+func (f *AuthorizedKeys) UnmarshalFlag(value string) error {
+	authorizedKeysBytes, err := ioutil.ReadFile(value)
+	if err != nil {
+		return fmt.Errorf("failed to read authorized keys: %s", err)
+	}
+
+	var authorizedKeys []ssh.PublicKey
+
+	for {
+		key, _, _, rest, err := ssh.ParseAuthorizedKey(authorizedKeysBytes)
+		if err != nil {
+			return fmt.Errorf("failed to parse authorized key: %s", err)
+		}
+
+		authorizedKeys = append(authorizedKeys, key)
+
+		authorizedKeysBytes = rest
+	}
+
+	f.Keys = authorizedKeys
+
+	return nil
+}
